091d330d5df2d473665b0b3b678c60f4cceda459
chuckstermajster <chuckstermajster@gmail.com>
2017-07-31 19:32:07 +0200

    Simple Timer. Need Improvement.

---
+++ Projekty C#/Timer/Timer/HelperClasses/RandomGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timer.HelperClasses
+{
+    class RandomGenerator
+    {
+        private static readonly Random random = new Random();
+
+        public static double generateRandomBeetwenTwoValues(double firstValue, double secondValue)
+        {
+            var randomNumber = random.NextDouble();
+            return (firstValue + (randomNumber * (secondValue - firstValue)))*1000;
+        }
+    }
+}
---
+++ Projekty C#/Timer/Timer/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Timer.HelperClasses;
+
+namespace Timer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private double getDoubleValueFromTextBox(TextBox textbox)
+        {
+            var textBoxValue = textbox.Text;
+            double textBoxIntValue = Double.Parse(textBoxValue);
+            return textBoxIntValue;
+
+        }
+
+        private double getRandomTimeBeetwenTwoValues()
+        {
+            var firstValue = getDoubleValueFromTextBox(StartValue);
+            var secondValue = getDoubleValueFromTextBox(EndValue);
+
+            return RandomGenerator.generateRandomBeetwenTwoValues(firstValue, secondValue);
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(getRandomTimeBeetwenTwoValues());
+            timer.Tick += Timer_Tick;
+            timer.Start();
+           
+
+            }
+
+
+
+
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Jupi, działa!");
+            DispatcherTimer timer = (DispatcherTimer)sender;
+            timer.Stop();
+        }
+    }
+}
